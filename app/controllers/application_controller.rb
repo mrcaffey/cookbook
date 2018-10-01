ba0a20aa5d0de960c2b74bc54649034c921bf18e
Micah Caffey <mrcaffey@gmail.com>
2018-10-01 10:19:07 -0600

    application controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def render_error(obj)
+    #obj.errors --> info about our errors  
+    #ing.errors.full_messages --> array of all of errors
+    render json: { errors: obj.errors.full_messages }, status: 422
+  end
 end
